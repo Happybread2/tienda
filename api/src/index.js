b1c68ea152cbed71f73b09dbbaee671630b02b80
Lukas Escobar <134743612+Lescobar2904@users.noreply.github.com>
2025-06-22 23:36:41 -0400

    subida

---
+++ api/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use('/api/usuarios', require('./routes/usuarios.routes'));
+
+// Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
